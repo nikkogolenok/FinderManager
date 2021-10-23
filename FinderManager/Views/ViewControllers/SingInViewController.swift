33f88b7cf8029fca75775f5b76c81bb5cb666694
nikita_kogolenok <nikiton17ns@gmail.com>
2021-10-24 00:43:34 +0300

    Start working with SingInViewController and UI

---
+++ FinderManager/Views/ViewControllers/SingInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SingInViewController.swift
+//  FinderManager
+//
+//  Created by Никита Коголенок on 24.10.21.
+//
+
+import UIKit
+
+class SingInViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
